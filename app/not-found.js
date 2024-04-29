2dc9f07be52d6c069ac0f59da14cd36bf901f5f2
João Flávio <jvflaviopinto@gmail.com>
2024-04-29 19:58:22 -0400

    Update not-found.js

--- app/not-found.js
+++ app/not-found.js
@@ -3,7 +3,7 @@ export default function notFound() {
   return (
     <div className='box'>
       <img alt='sigh' className='gif' src='/sigh.gif' />
-      Page not found
+      Página não encontrada
     </div>
   )
   
